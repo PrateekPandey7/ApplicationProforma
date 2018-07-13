b93c6c01ba83fdf204a9badb81b84cb0b56a69f9
PrateekPandey7 <pandeyprateek01@gmail.com>
2018-07-13 11:47:40 +0530

    Changes

--- formtopdf.php
+++ formtopdf.php
@@ -579,7 +579,7 @@ $pdf->Ln(30);
 $pdf->Cell(100,10,'Terms and Conditions as specified in the Online Application Proforma/ related notification on NPC website');
 $pdf->Ln(15);
 $pdf->SetFont('Arial','',12);
-$pdf->MultiCell(270,7,"I hereby verify that the information provided by me above is true and correct and my application can be summarily rejected if it is found incorrect. I shall further be liable to other legal action. Further, I have read through the terms and conditions and understood them. I agree to those terms and conditions.",0,'L',false);
+$pdf->MultiCell(270,7,"I hereby verify that the information provided by me above is true and correct and my application can be summarily rejected if it is found incorrect. I shall further be liable to other legal action. Further, I have read through the terms and conditions and understood them. I agree to these terms and conditions.",0,'L',false);
 
 //Place
 $pdf->SetFont('Arial','B',14);
